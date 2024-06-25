64ba008442ad2d887b7a9f6691836885d0e2b511
Aidan James <ajame23@uwo.ca>
2024-06-25 15:06:49 -0400

    working app

---
+++ foodie/Landing.swift
@@ -0,0 +1,18 @@
+//
+//  Landing.swift
+//  foodie
+//
+//  Created by Aidan James on 2024-06-25.
+//
+
+import SwiftUI
+
+struct Landing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Landing()
+}
